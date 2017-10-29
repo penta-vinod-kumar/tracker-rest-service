d90270eff118b5be73e5bd45e9424d93d7bd2d58
t234722 <vinod.penta@klm.com>
2017-10-29 11:33:24 +0530

    Added id to user info

--- src/main/java/com/gohiram/haj/trackerrestservice/model/UserInformation.java
+++ src/main/java/com/gohiram/haj/trackerrestservice/model/UserInformation.java
@@ -1,34 +1,36 @@
 package com.gohiram.haj.trackerrestservice.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name="Users")
-@Table(name="Users")
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Users")
+@Table(name = "Users")
 @ApiModel
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserInformation {
 
-	@ApiModelProperty
-	private String firstName;
-	
-	@ApiModelProperty
-	private String lastName;
-	
-	@ApiModelProperty
-	private String emailId;
-	
-	@Id
-	@ApiModelProperty
-	private long mobileNumber;
-	
+    @Id
+    private String id;
+
+    @ApiModelProperty
+    private String firstName;
+
+    @ApiModelProperty
+    private String lastName;
+
+    @ApiModelProperty
+    private String emailId;
+
+    @ApiModelProperty
+    private long mobileNumber;
+
 }
